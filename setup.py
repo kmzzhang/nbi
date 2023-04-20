c3c1cd73cd80d5f306d77f6ab79772a85afc6dcf
kmzzhang <keming96822@hotmail.com>
2023-04-20 12:26:03 -0700

    fix merge conflict

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
-from setuptools import setup, find_packages
+import codecs
 import os
+import re
 
+from setuptools import setup, find_packages
+
+###################################################################
+
+NAME = "nbi"
 PACKAGES = find_packages(where="src")
+META_PATH = os.path.join("src", "nbi", "__init__.py")
+CLASSIFIERS = [
+    "Development Status :: 2 - Pre-Alpha",
+    "License :: OSI Approved :: BSD License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python"
+]
 
 INSTALL_REQUIRES = [
     "torch>=1.4",
@@ -11,18 +24,60 @@ INSTALL_REQUIRES = [
     "corner",
     "joblib",
     "pytest",
-    "tqdm>=4.64.1",
-    'wandb',
-    'multiprocess',
-    'ipywidgets'
+    "tqdm",
+    'multiprocess'
 ]
 
-setup(
-    name='nbi',
-    author='Keming Zhang',
-    author_email='kemingz@berkeley.edu',
-    packages=PACKAGES,
-    package_dir={"": "src"},
-    version='0.1.1',
-    install_requires=INSTALL_REQUIRES
-)
+###################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """
+    Build an absolute path from *parts* and and return the contents of the
+    resulting file.  Assume UTF-8 encoding.
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as f:
+        return f.read()
+
+
+META_FILE = read(META_PATH)
+
+
+def find_meta(meta):
+    """
+    Extract __*meta*__ from META_FILE.
+    """
+    # match strings that starts and ends with either a single 
+    # or double quote and contains any number of characters 
+    # that are not quotes.
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError("Unable to find __{meta}__ string.".format(meta=meta))
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        author=find_meta("author"),
+        author_email=find_meta("email"),
+        maintainer=find_meta("author"),
+        maintainer_email=find_meta("email"),
+        url=find_meta("url"),
+        license=find_meta("license"),
+        description=find_meta("description"),
+        long_description=read("README.md"),
+        long_description_content_type="text/markdown",
+        packages=PACKAGES,
+        package_dir={"": "src"},
+        include_package_data=True,
+        python_requires=">=3.7",
+        install_requires=INSTALL_REQUIRES,
+        classifiers=CLASSIFIERS,
+        zip_safe=True,
+    )
